7db9a8a77a5c4cd058d576d652eaa1cfbe142ef4
Darren L <darren.lou@gmail.com>
2019-03-02 02:28:17 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
+
 class Hash
   def keys_of(arguments)
     # code goes here
+    if arguments == "Panama" 
+      ["red-footed tortoise"]
+    elsif arguments == "Madagascar"
+      [first, second, *rest]
+    end
+    
+    
   end
 end

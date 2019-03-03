11a5a96a5798671623fafc7fa49bf54ba764f903
Darren L <darren.lou@gmail.com>
2019-03-03 14:31:07 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,16 +3,11 @@ require "pry"
 # { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
 
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     result = []
-    a, *b, c = arguments
-    if a == "Panama"
-      result << key(a)
-    elsif a == "Madagascar"
-    binding.pry
-      result << key(a)
+    if arguments == "Panama"
+      binding.pry
+      result = self.key(arguments)
     end
-    result
-
   end
 end

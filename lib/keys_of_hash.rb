ca96e444fb0ff399ea1ee8128b7e239da3650c1b
Darren L <darren.lou@gmail.com>
2019-03-04 19:21:10 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,12 +2,16 @@ require "pry"
 
 # { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
 
+# {a: 1, b: 2, c: 3}
+
 class Hash
   def keys_of(*arguments)
     self.collect { |key, value|
       if arguments.join(", ") == value
         binding.pry
         key
+      end
+      if arguments
       end
     }.compact
     

2d4a1815140d5fed74da81c41755ec1f534b6253
Darren L <darren.lou@gmail.com>
2019-03-04 20:01:15 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,5 @@
 require "pry"
 
-# { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
-
-# {a: 1, b: 2, c: 3}
-
 class Hash
   def keys_of(*arguments)
     self.collect { |key, value|
